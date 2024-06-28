a787afc37dd6492bfc663b3175f9ca06fcdf5fdd
kvkhang <dathee2@gmail.com>
2024-06-27 17:55:14 -0700

    ipynb is cool

--- StockScrape/datacollect.py
+++ StockScrape/datacollect.py
@@ -1,5 +1,8 @@
 import yfinance as yf # type: ignore
+import matplotlib as plot #type: ignore
 
 stock = yf.Ticker("NVDA")
 
 stock = stock.history(period="max")
+
+stock.plot.line(y="Close", use_index=True)
