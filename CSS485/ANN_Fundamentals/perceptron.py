842241196756d7bef2f501e9165ef8bb0f309293
Nosua <dathee2@gmail.com>
2025-02-08 17:32:54 -0800

    question 2, missing backward

--- CSS485/ANN_Fundamentals/perceptron.py
+++ CSS485/ANN_Fundamentals/perceptron.py
@@ -1,10 +1,12 @@
 import numpy as np
 import transferFunctions as tf
 
+
 class PerceptronLayer:
     """
     A single Perceptron Layer
     """
+
     """
     Creates a Perceptron Layer from either predetermined or random weights
 
@@ -13,23 +15,23 @@ class PerceptronLayer:
         jb(int or np.ndarray): outputs/bias matrix
         tfname(string): sets the transfer function type
     """
+
     def __init__(self, iw, jb, tfname):
         if isinstance(iw, int) and isinstance(jb, int):
             self.weights = np.random.uniform(-1, 1, size=(jb, iw))
             self.bias = np.random.uniform(-1, 1, size=(jb))
-            
-            
+
         elif isinstance(iw, np.ndarray) and isinstance(jb, np.ndarray):
             if len(jb) != len(iw):
                 return ValueError("Bias must be only 1 row")
             self.weights = iw
             self.bias = jb
-        
+
         else:
             return ValueError("Input 2 Scalars or 2 Matrixes")
-        
+
         self.tf = getattr(tf, tfname)
-    
+
     """
     Does a forward pass with the given inputs through a single layer and returns results
     
@@ -38,9 +40,10 @@ class PerceptronLayer:
     
     Returns:
         output(np.ndarray): All outputs in one array
-    """    
+    """
+
     def forwardLoop(self, inputs):
-        if (len(inputs) != len(self.weights[0])):
+        if len(inputs) != len(self.weights[0]):
             return ValueError(f"You need {len(self.weights)} inputs")
         output = np.zeros(len(self.bias))
         i = 0
@@ -50,7 +53,7 @@ class PerceptronLayer:
             output[i] = self.tf(b)
             i += 1
         return output
-    
+
     """
     Does a forward pass with the given inputs through a single layer and returns results
     
@@ -60,8 +63,25 @@ class PerceptronLayer:
     Returns:
         output(np.ndarray): All outputs in one array
     """
+
     def forward(self, inputs):
-        if (len(inputs) != len(self.weights[0])):
+        if len(inputs) != len(self.weights[0]):
             return ValueError(f"You need {len(self.weights)} inputs")
         output = (self.weights @ inputs) + self.bias
         return self.tf(output)
+
+    def errorLoss(self, a, t):
+        if len(a) != len(t):
+            return ValueError("Length MisMatch")
+        return t - a
+
+    def backward(self, errors):
+        pass
+
+    def print(self):
+        print("Layer:")
+        i = 1
+        for x, b in zip(self.weights, self.bias):
+            print(f"   w{i} = {x}\tb = {b}")
+            i += 1
+        pass
